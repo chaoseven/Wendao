996a71f1453a25463340d16c1c2aae7143c2ff2a
chao <chao@CHAO-PC>
2016-11-04 00:28:42 +0800

    添加项目文件。

---
+++ src/Wendao.App/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Wendao.App.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Wendao.App.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Signin(string returnUrl)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Signin(LoginViewModel loginInfo,string returnUrl)
+        {
+            return Redirect(returnUrl);
+        }
+    }
+}
